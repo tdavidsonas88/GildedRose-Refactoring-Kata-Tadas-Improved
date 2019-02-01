c56a0b5386c1cdbbeb00b7ef078f70c61d31c2f7
Tadas <tdavidsonas88@gmail.com>
2019-02-01 20:04:58 +0200

    #master - task description

--- tests/gilded_rose_test.php
+++ tests/gilded_rose_test.php
@@ -11,7 +11,7 @@ final class GildedRoseTest extends TestCase {
         $items = array(new Item("foo", 0, 0));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals("foo", $items[0]->name);
     }
 
 }
