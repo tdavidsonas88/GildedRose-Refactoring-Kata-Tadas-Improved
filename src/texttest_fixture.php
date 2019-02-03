e9069a38b5637f85f06759233bd036624bb1bf8b
Tadas <tdavidsonas88@gmail.com>
2019-02-03 17:15:31 +0200

    #master - testOnceTheSellByDateHasPassedQualityDegradesTwiceAsFast

--- src/texttest_fixture.php
+++ src/texttest_fixture.php
@@ -13,7 +13,6 @@ $items = array(
     new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
     new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
     new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
     new Item('Conjured Mana Cake', 3, 6)
 );
 
@@ -31,7 +30,7 @@ for ($i = 0; $i < $days; $i++) {
         echo $item . PHP_EOL;
     }
     echo PHP_EOL;
-    
+
     // At the end of each day our system lowers both values for every item
     $app->update_quality();
 }
--- tests/gilded_rose_test.php
+++ tests/gilded_rose_test.php
@@ -8,10 +8,28 @@ use PHPUnit\Framework\TestCase;
 final class GildedRoseTest extends TestCase {
 
     function testFoo() {
-        $items = array(new Item("foo", 0, 0));
+        $items = array(new Item("foo", 2, 5));
         $gildedRose = new GildedRose($items);
         $gildedRose->update_quality();
         $this->assertEquals("foo", $items[0]->name);
+        $this->assertEquals(1, $items[0]->sell_in);
+        $this->assertEquals(4, $items[0]->quality);
+    }
+
+    // - Once the sell by date has passed, Quality degrades twice as fast
+    function testOnceTheSellByDateHasPassedQualityDegradesTwiceAsFast(){
+        $items = array(new Item("foo", 2, 5));
+
+        $gildedRose = new GildedRose($items);
+
+        $gildedRose->update_quality();
+        $this->assertEquals(4, $items[0]->quality);
+
+        $gildedRose->update_quality();
+        $this->assertEquals(3, $items[0]->quality);
+
+        $gildedRose->update_quality();
+        $this->assertEquals(1, $items[0]->quality);
     }
 
 }
